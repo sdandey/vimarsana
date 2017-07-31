c0d92c727222dcb784e930805ecf77f90bab6ecd
mirunalini1206 <mirunalini1206@gmail.com>
2017-07-30 22:11:22 -0500

    modified the script to retrieve event information for more countries \n Modified code to read access token information from database \n modified code to retrieve new access token from the list if FB API hits error

--- extract_city_in_india.py
+++ extract_city_in_india.py
@@ -1,7 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
-url = 'https://en.wikipedia.org/wiki/List_of_cities_in_India_by_population'
+url = 'https://en.wikipedia.org/wiki/List_of_English_districts_by_population'
+
+def get_wiki_url_for_each_country():
+    city_extract_url ={}
+    city_extract_url['india'] = 'https://en.wikipedia.org/wiki/List_of_cities_in_India_by_population'
+    city_extract_url['usa'] = 'https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population'
+    city_extract_url['canada'] = 'https://en.wikipedia.org/wiki/List_of_the_100_largest_municipalities_in_Canada_by_population'
+    city_extract_url['uk'] = 'https://en.wikipedia.org/wiki/List_of_English_districts_by_population'
+    return city_extract_url
 
 
 def extract_cities_information_from_wiki(url):
@@ -18,11 +26,13 @@ def extract_cities_information_from_wiki(url):
                 rows.append([ele for ele in cols if ele])
     cities = list()
     for row in rows:
-        if (len(row) > 4):
+        if (len(row) >= 4):
             cities.append(row[1])
     return cities
 
+
 cities = extract_cities_information_from_wiki(url)
+print ('total count:' + str(len(cities)))
 for city in cities:
     print (city)
 
